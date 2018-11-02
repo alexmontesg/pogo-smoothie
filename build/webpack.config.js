35ac853798f513b4563c5dbf69892ab6b9ab0666
Alejandro Montes Garcia <alejandro@MacBook-Air-de-Alejandro.local>
2018-11-02 23:25:12 +0100

    Add build script to package

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  entry: ['../resources/scripts/main.js'],
+  entry: ['./resources/scripts/main.js'],
   output: {
-    filename: '../../public/bundle.js'
+    filename: './../public/bundle.js'
   },
   mode: 'development',
   module: {
